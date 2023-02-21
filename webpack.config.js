f26f99c601f69337ecffe180247f8b81573c79fa
김민지 <kimmj1@focusmediakorea.com>
2023-02-21 15:33:41 +0900

    웹팩 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+const path = require("path");
 
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:8080/",
+    path: path.resolve(__dirname, "dist"),
   },
 
   resolve: {
