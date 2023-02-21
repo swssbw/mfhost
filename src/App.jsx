689ab7c24b89ee0d765f0ac3b79b47148c315e36
김민지 <kimmj1@focusmediakorea.com>
2023-02-21 16:31:17 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+
+const Device = React.lazy(() => import("device/app"));
 
 const App = () => (
   <div className="container">
-    <h1>HOST</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/device" element={<Device />} />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
