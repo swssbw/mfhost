8bc333e6778022b27b8e976b1e62987b97c092a5
김민지 <kimmj1@focusmediakorea.com>
2023-02-21 16:04:50 +0900

    번들 크기 확인

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import DeviceApp from "device/App";
-import ComplexApp from "complex/App";
 
 const App = () => (
   <div className="container">
     <h1>HOST</h1>
-    <DeviceApp />
-    <ComplexApp />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
