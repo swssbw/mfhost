7427aadb8400fa861c21fdbb6abb227dcfd8bf12
김민지 <kimmj1@focusmediakorea.com>
2023-02-21 15:48:32 +0900

    리모트앱 url 수정

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-window.deviceURL = "https://main.dn9ei2y33oj44.amplifyapp.com/";
-window.complexURL = "https://main.d24avdyww3naaf.amplifyapp.com/";
+window.deviceURL = "https://main.dn9ei2y33oj44.amplifyapp.com";
+window.complexURL = "https://main.d24avdyww3naaf.amplifyapp.com";
 import("./App");
